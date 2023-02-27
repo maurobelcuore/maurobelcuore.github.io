be5549360a29b893ca7eed2223524083f184a903
maurobelcuore <126527321+maurobelcuore@users.noreply.github.com>
2023-02-27 20:52:22 -0300

    Initial commit

---
+++ funcion.js
@@ -0,0 +1,28 @@
+
+
+// Botones - datos personales
+
+document.getElementById('boton1').addEventListener('click', function(){
+    document.getElementById("dato1").innerHTML = "<b>37 años</b>"; 
+    } );
+
+document.getElementById('boton2').addEventListener('click', function(){
+    document.getElementById("dato2").innerHTML = "<b>Avenida Falsa 123, Buenos Aires</b>"; 
+        } );
+
+document.getElementById('boton3').addEventListener('click', function(){
+    document.getElementById("dato3").innerHTML = "<b>+54-9-11-5555-5555</b>"; 
+    } );
+
+
+document.getElementById('boton4').addEventListener('click', function(){
+        document.getElementById("dato4").innerHTML = "<b> maurobel@micorreo.com </b>"; 
+        } );
+
+
+
+
+
+
+
+
